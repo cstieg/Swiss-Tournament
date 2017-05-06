00473ca59123ae3da6117c4ccc131ccd01cf3c23
cstieg <cstieg4899@yahoo.com>
2017-05-06 22:02:03 +0900

    feat: Add delete cascade functionality
    Deleting a playing needs to cascade the delete to any contests of which the player was a part.
    Also player standings added as a view.

--- tournament.sql
+++ tournament.sql
@@ -6,6 +6,8 @@
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
 
+DELETE DATABASE IF EXISTS tournament;
+
 CREATE DATABASE tournament;
 
 \c tournament
@@ -17,6 +19,18 @@ CREATE TABLE player (
 
 CREATE TABLE contest (
 	ContestID			SERIAL PRIMARY KEY,
-	Winner				INTEGER REFERENCES player(PlayerID),
-	Loser				INTEGER REFERENCES player(PlayerID)
+	Winner				INTEGER REFERENCES player(PlayerID) ON DELETE CASCADE,
+	Loser				INTEGER REFERENCES player(PlayerID) ON DELETE CASCADE
+	CHECK (winner <> loser)
 );
+
+CREATE VIEW player_standings AS \
+SELECT player.PlayerID, player.name, \
+                    COUNT(CASE contest.Winner WHEN player.PlayerID THEN 1 END) as wins, \
+                    COUNT(CASE WHEN player.PlayerID = contest.Winner \
+                               OR player.PlayerID = contest.Loser THEN 1 END) as matches \
+                    FROM player \
+                    LEFT JOIN contest ON player.PlayerID = contest.Winner \
+                                      OR player.PlayerID = contest.Loser \
+                    GROUP BY player.PlayerID, player.name \
+                    ORDER BY wins DESC;
